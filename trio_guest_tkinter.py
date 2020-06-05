d87c175828a0cfa0083b8a2f862354855ac5fdb5
Richard Sheridan <richard.sheridan@gmail.com>
2020-06-05 11:45:57 -0400

    Refactor to match trio guest api

--- trio_guest_tkinter.py
+++ trio_guest_tkinter.py
@@ -1,5 +1,6 @@
 #
 # Copyright 2018 Roger D. Serwy
+# Modifications Copyright 2020 Richard J. Sheridan
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,45 +15,15 @@
 # limitations under the License.
 #
 
-"""
-tkthread
---------
-Easy multithreading with Tkinter on CPython 2.7/3.x and PyPy 2.7/3.x.
-Background
-----------
-Multithreading with `Tkinter` can often cause the following errors:
-    RuntimeError: main thread is not in main loop
-    RuntimeError: Calling Tcl from different apartment
-    NotImplementedError: Call from another thread
-This module allows Python multithreading to cooperate with Tkinter.
-Usage
------
-The `tkthread` module provides the `TkThread` class,
-which can synchronously interact with the main thread.
-    from tkthread import tk, TkThread
-    root = tk.Tk()        # create the root window
-    tkt = TkThread(root)  # make the thread-safe callable
-    import threading, time
-    def run(func):
-        threading.Thread(target=func).start()
-    run(lambda:     root.wm_title('FAILURE'))
-    run(lambda: tkt(root.wm_title,'SUCCESS'))
-    root.update()
-    time.sleep(2)  # _tkinter.c:WaitForMainloop fails
-    root.mainloop()
-There is an optional `.install()` method on `TkThread` which
-intercepts Python-to-Tk calls. This must be called on the
-default root, before the creation of child widgets. There is
-a slight performance penalty for Tkinter widgets that operate only
-on the main thread.
-"""
-import functools
 import threading
 import tkinter as tk
 import queue
+import traceback
+
+from outcome import Error
 
 
-class TkThread(object):
+class TkGuest:
     def __init__(self, root):
         """TkThread object for the root 'tkinter.Tk' object"""
 
@@ -65,15 +36,12 @@ class TkThread(object):
         self._call_from_name = self.root.register(self._call_from)
         self._thread_queue = queue.SimpleQueue()
 
-        self._running = True
         self._th = threading.Thread(target=self._tcl_thread)
-        self._th.daemon = True
         self._th.start()
 
     def _call_from(self):
         # This executes in the main thread, called from the Tcl interpreter
-        func, args, kwargs, tres = self._call_from_data.pop(0)
-        func(*args, **kwargs)
+        self._call_from_data.pop(0)()
 
     def _tcl_thread(self):
         # Operates in its own thread, with its own Tcl interpreter
@@ -81,21 +49,26 @@ class TkThread(object):
         tcl = tk.Tcl()
         tcl.eval('package require Thread')
 
-        command = 'thread::send  %s "%s"' % (self._main_thread_id,
-                                             self._call_from_name)
-        while self._running:
+        command = f'thread::send  {self._main_thread_id} "{self._call_from_name}"'
+
+        while True:
             item = self._thread_queue.get()
             if item is None:
                 break
             self._call_from_data.append(item)
             tcl.eval(command)
 
-    def nosync(self, func, *args, **kwargs):
+    def run_sync_soon_threadsafe(self, func):
         """Non-blocking, no-synchronization """
-        self._thread_queue.put((func, args, kwargs, None))
+        self._thread_queue.put(func)
 
-    def destroy(self):
-        """Destroy the TkThread object.
+    def done_callback(self, outcome):
+        """End the Tcl Thread and the Tk app.
         """
-        self._running = False
+        print(f"Outcome: {outcome}")
+        if isinstance(outcome, Error):
+            exc = outcome.error
+            traceback.print_exception(type(exc), exc, exc.__traceback__)
         self._thread_queue.put(None)  # unblock _tcl_thread queue
+        self._th.join()
+        self.root.destroy()
