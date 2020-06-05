61c1321ca7134e0170baa1b1038a090ee225d25f
Richard Sheridan <richard.sheridan@gmail.com>
2020-06-05 12:31:11 -0400

    Refactor to guest class

--- trio_guest_qt5.py
+++ trio_guest_qt5.py
@@ -11,11 +11,52 @@ import traceback
 # https://bugreports.qt.io/projects/PYSIDE/issues/PYSIDE-1313
 from PyQt5 import QtCore, QtWidgets
 
-FPS = 60
-
 import httpcore._async.http11
+
 # Default is 4096
 httpcore._async.http11.AsyncHTTP11Connection.READ_NUM_BYTES = 100_000
+
+FPS = 60
+
+# class Reenter(QtCore.QObject):
+#     run = QtCore.Signal(object)
+#
+# This is substantially faster than using a signal... for some reason Qt
+# signal dispatch is really slow (and relies on events underneath anyway, so
+# this is strictly less work)
+REENTER_EVENT = QtCore.QEvent.Type(QtCore.QEvent.registerEventType())
+
+
+class ReenterEvent(QtCore.QEvent):
+    pass
+
+
+class Reenter(QtCore.QObject):
+    def event(self, event):
+        event.fn()
+        return False
+
+
+class QtGuest:
+    def __init__(self, app):
+        self.app = app
+        self.reenter = Reenter()
+        # or if using Signal
+        # self.reenter.run.connect(lambda fn: fn(), QtCore.Qt.QueuedConnection)
+        # self.run_sync_soon_threadsafe = self.reenter.run.emit
+
+    def run_sync_soon_threadsafe(self, fn):
+        event = ReenterEvent(REENTER_EVENT)
+        event.fn = fn
+        self.app.postEvent(self.reenter, event)
+
+    def done_callback(self, outcome):
+        print(f"Outcome: {outcome}")
+        if isinstance(outcome, Error):
+            exc = outcome.error
+            traceback.print_exception(type(exc), exc, exc.__traceback__)
+        self.app.quit()
+
 
 async def get(url):
     dialog = QtWidgets.QProgressDialog(f"Fetching {url}...", "Cancel", 0, 0)
@@ -32,7 +73,7 @@ async def get(url):
                 dialog.setMaximum(total)
                 async for chunk in response.aiter_raw():
                     downloaded += len(chunk)
-                    if time.monotonic() - last_screen_update > 1/FPS:
+                    if time.monotonic() - last_screen_update > 1 / FPS:
                         dialog.setValue(downloaded)
                         last_screen_update = time.monotonic()
         end = time.monotonic()
@@ -42,47 +83,15 @@ async def get(url):
         print(f"{bytes_per_sec:.2f} bytes/sec")
     return 1
 
-app = QtWidgets.QApplication(sys.argv)
 
-# class Reenter(QtCore.QObject):
-#     run = QtCore.Signal(object)
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    guest = QtGuest(app)
+    trio.lowlevel.start_guest_run(
+        get,
+        sys.argv[1],
+        run_sync_soon_threadsafe=guest.run_sync_soon_threadsafe,
+        done_callback=guest.done_callback,
+    )
 
-# reenter = Reenter()
-# reenter.run.connect(lambda fn: fn(), QtCore.Qt.QueuedConnection)
-# run_sync_soon_threadsafe = reenter.run.emit
-
-# This is substantially faster than using a signal... for some reason Qt
-# signal dispatch is really slow (and relies on events underneath anyway, so
-# this is strictly less work)
-REENTER_EVENT = QtCore.QEvent.Type(QtCore.QEvent.registerEventType())
-
-class ReenterEvent(QtCore.QEvent):
-    pass
-
-class Reenter(QtCore.QObject):
-    def event(self, event):
-        event.fn()
-        return False
-
-reenter = Reenter()
-
-def run_sync_soon_threadsafe(fn):
-    event = ReenterEvent(REENTER_EVENT)
-    event.fn = fn
-    app.postEvent(reenter, event)
-
-def done_callback(outcome):
-    print(f"Outcome: {outcome}")
-    if isinstance(outcome, Error):
-        exc = outcome.error
-        traceback.print_exception(type(exc), exc, exc.__traceback__)
-    app.quit()
-
-trio.lowlevel.start_guest_run(
-    get,
-    sys.argv[1],
-    run_sync_soon_threadsafe=run_sync_soon_threadsafe,
-    done_callback=done_callback,
-)
-
-app.exec_()
+    app.exec_()
