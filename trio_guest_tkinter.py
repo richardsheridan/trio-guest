3c43efad0eafe18bbf2ad6c54d2184ef3e15c640
Richard Sheridan <richard.sheridan@gmail.com>
2020-06-07 09:26:57 -0400

    No longer a derivative work of tkthread

--- trio_guest_tkinter.py
+++ trio_guest_tkinter.py
@@ -1,6 +1,5 @@
 #
-# Copyright 2018 Roger D. Serwy
-# Modifications Copyright 2020 Richard J. Sheridan
+# Copyright Copyright 2020 Richard J. Sheridan
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
