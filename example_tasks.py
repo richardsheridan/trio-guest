d99f23946a8d181800728809682743b4b7e5f8b8
Richard Sheridan <richard.sheridan@gmail.com>
2020-07-08 15:36:17 -0400

    Add win32 host

--- example_tasks.py
+++ example_tasks.py
@@ -13,8 +13,12 @@ httpcore._async.http11.AsyncHTTP11Connection.READ_NUM_BYTES = 100_000
 
 
 async def get(display):
-    size_guess = 1024 * 1024 * 6 // 5
-    url = sys.argv[1]
+    try:
+        url = sys.argv[1]
+        size_guess = int(sys.argv[2])
+    except IndexError:
+        url = "http://google.com/"
+        size_guess = 5269
     fps = 60
     display.set_title(f"Fetching {url}...")
     with trio.CancelScope() as cscope:
@@ -23,14 +27,23 @@ async def get(display):
         downloaded = 0
         last_screen_update = time.monotonic()
         async with httpx.AsyncClient() as client:
-            async with client.stream("GET", url) as response:
-                total = int(response.headers.get("content-length", size_guess))
-                display.set_max(total)
-                async for chunk in response.aiter_raw():
-                    downloaded += len(chunk)
-                    if time.monotonic() - last_screen_update > 1 / fps:
-                        display.set_value(downloaded)
-                        last_screen_update = time.monotonic()
+            for i in range(10):
+                print("Connection attempt", i)
+                try:
+                    async with client.stream("GET", url) as response:
+                        total = int(response.headers.get("content-length", size_guess))
+                        display.set_max(total)
+                        async for chunk in response.aiter_raw():
+                            downloaded += len(chunk)
+                            if time.monotonic() - last_screen_update > 1 / fps:
+                                display.set_value(downloaded)
+                                last_screen_update = time.monotonic()
+                    break
+                except httpcore._exceptions.ReadTimeout:
+                    pass
+            else:
+                print("response timed out 10 times")
+                return
         end = time.monotonic()
         dur = end - start
         bytes_per_sec = downloaded / dur
---
+++ trio_guest_win32.py
@@ -0,0 +1,189 @@
+#
+# Copyright 2020 Richard J. Sheridan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import traceback
+
+import timer  # from pywin32
+import tqdm
+import trio
+import win32api
+import win32con
+import win32event
+import win32gui
+import win32ui
+from outcome import Error
+from pywin.mfc import dialog
+
+from example_tasks import get
+
+TRIO_MSG = win32con.WM_USER + 3
+
+
+class Win32Host:
+    def __init__(self, shutdown):
+        global trio_functions
+        self.shutdown = shutdown
+        self.funcs = trio_functions
+        self.mainthreadid = win32api.GetCurrentThreadId()
+        # create event queue with null op
+        win32gui.PeekMessage(
+            win32con.NULL, win32con.WM_USER, win32con.WM_USER, win32con.PM_NOREMOVE
+        )
+
+    def run_sync_soon_threadsafe(self, func):
+        """Use use PostThreadMessage to schedule a callback
+        https://docs.microsoft.com/en-us/windows/win32/winmsg/about-messages-and-message-queues
+        """
+        fid = id(func)
+        win32api.PostThreadMessage(self.mainthreadid, TRIO_MSG, fid, win32con.NULL)
+        self.funcs[fid] = func
+
+    def run_sync_soon_not_threadsafe(self, func):
+        """Use use PostMessage to schedule a callback
+        https://docs.microsoft.com/en-us/windows/win32/winmsg/about-messages-and-message-queues
+        This doesn't provide any real efficiency over threadsafe.
+        """
+        fid = id(func)
+        win32api.PostMessage(win32con.NULL, TRIO_MSG, fid, win32con.NULL)
+        self.funcs[fid] = func
+
+    def done_callback(self, outcome):
+        """non-blocking request to end the main loop
+        """
+        print(f"Outcome: {outcome}")
+        if isinstance(outcome, Error):
+            exc = outcome.error
+            traceback.print_exception(type(exc), exc, exc.__traceback__)
+            exitcode = 1
+        else:
+            exitcode = 0
+        self.shutdown(exitcode)
+
+
+def MakeDlgTemplate():
+    style = (
+        win32con.DS_MODALFRAME
+        | win32con.WS_POPUP
+        | win32con.WS_VISIBLE
+        | win32con.WS_CAPTION
+        | win32con.WS_SYSMENU
+        | win32con.DS_SETFONT
+    )
+    cs = win32con.WS_CHILD | win32con.WS_VISIBLE
+
+    w = 300
+    h = 21
+
+    dlg = [
+        ["...", (0, 0, w, h), style, None, (8, "MS Sans Serif")],
+    ]
+
+    s = win32con.WS_TABSTOP | cs
+
+    dlg.append(
+        [128, "Cancel", win32con.IDCANCEL, (w - 60, h - 18, 50, 14), s | win32con.BS_PUSHBUTTON]
+    )
+
+    return dlg
+
+
+class PBarDialog(dialog.Dialog):
+    def OnInitDialog(self):
+        code = super().OnInitDialog()
+        self.pbar = win32ui.CreateProgressCtrl()
+        self.pbar.CreateWindow(
+            win32con.WS_CHILD | win32con.WS_VISIBLE, (10, 10, 310, 24), self, 3000
+        )
+        return code
+
+    def OnCancel(self):
+        # also window close response
+        self.cancelfn()
+
+
+class Win32Display:
+    def __init__(self):
+        self.display = PBarDialog(MakeDlgTemplate())
+        self.display.CreateWindow()
+        # self.display.DoModal()
+
+    def set_title(self, title):
+        self.display.SetWindowText(title)
+
+    def set_max(self, maximum):
+        # hack around uint16 issue
+        self.realmax = maximum
+        self.display.pbar.SetRange(0, 65535)
+
+    def set_value(self, downloaded):
+        self.display.pbar.SetPos(int((downloaded / self.realmax * 65535)))
+
+    def set_cancel(self, fn):
+        self.display.cancelfn = fn
+
+    def shutdown(self, error_code):
+        self.display.PostMessage(win32con.WM_CLOSE, 0, 0)
+        self.display.close()
+        win32gui.PostQuitMessage(error_code)
+
+
+trio_functions = {}
+
+
+# @cffi.def_extern()  # if your mainloop is in C/C++
+def do_trio(lparam):
+    try:
+        trio_functions.pop(lparam)()
+    except KeyError:  # redundant posted messages
+        pass
+
+
+def main(task):
+    display = Win32Display()
+    host = Win32Host(display.shutdown)
+    trio.lowlevel.start_guest_run(
+        task,
+        display,
+        run_sync_soon_threadsafe=host.run_sync_soon_threadsafe,
+        run_sync_soon_not_threadsafe=host.run_sync_soon_not_threadsafe,
+        done_callback=host.done_callback,
+    )
+    while True:
+        code, msg = win32gui.GetMessage(0, 0, 0)
+        if not code:
+            break
+        if code < 0:
+            error = win32api.GetLastError()
+            raise RuntimeError(error)
+
+        #######################################
+        ### Trio specific part of main loop ###
+        #######################################
+        hwnd, msgid, lparam, wparam, time, point = msg
+        if hwnd == win32con.NULL and msgid == TRIO_MSG:
+            do_trio(lparam)
+            continue
+        ###############################
+        ### Trio specific part ends ###
+        ###############################
+
+        win32gui.TranslateMessage(msg)
+        win32gui.DispatchMessage(msg)
+
+
+if __name__ == "__main__":
+    print("Known bug: Dragging the window freezes everything.")
+    print("For now only click buttons!")
+    main(get)
