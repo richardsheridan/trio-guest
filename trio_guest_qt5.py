c6c474cc973df052b25f1eeb58bcc6a182702761
Richard Sheridan <richard.sheridan@gmail.com>
2020-07-23 09:34:18 -0400

    qt clarification

--- trio_guest_qt5.py
+++ trio_guest_qt5.py
@@ -31,7 +31,7 @@ from example_tasks import get
 # This is substantially faster than using a signal... for some reason Qt
 # signal dispatch is really slow (and relies on events underneath anyway, so
 # this is strictly less work)
-REENTER_EVENT = QtCore.QEvent.Type(QtCore.QEvent.registerEventType())
+REENTER_EVENT_TYPE = QtCore.QEvent.Type(QtCore.QEvent.registerEventType())
 
 
 class ReenterEvent(QtCore.QEvent):
